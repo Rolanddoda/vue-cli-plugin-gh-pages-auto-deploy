0876ec21c86fd9689a8fef9c6f72e4b463c135f5
Roland Doda <rolanddoda2014@gmail.com>
2020-04-26 11:28:50 +0200

    Fixes + update/create `vue.config.js`

--- generator/helpers.js
+++ generator/helpers.js
@@ -10,15 +10,15 @@ function addFiles(api, opts) {
   api.render(files, opts)
 }
 
-async function addUserNameAndEmail (api) {
+async function addUserNameAndEmail(api) {
   const filePath = api.resolve('.github/workflows/gh-pages-deploy.yml')
-  const file = fs.readFileSync(filePath, { encoding: 'utf8' })
-  const { username, email } = getUserCredentials()
+  const file = fs.readFileSync(filePath, {encoding: 'utf8'})
+  const {username, email} = getUserCredentials()
   const updatedFile = file.replace('[[username]]', username).replace('[[email]]', email)
-  fs.writeFileSync(filePath, updatedFile, { encoding: 'utf8' })
+  fs.writeFileSync(filePath, updatedFile, {encoding: 'utf8'})
 }
 
-function extendPackage (api) {
+function extendPackage(api) {
   api.extendPackage({
     scripts: {
       "gh-pages-deploy": "node scripts/gh-pages-deploy.js"
@@ -31,7 +31,27 @@ function extendPackage (api) {
   })
 }
 
-async function getUserCredentials () {
+async function createOrUpdateVueConfig(api) {
+  const config = api.resolve('vue.config.js')
+
+  if (!fs.existsSync(config)) {
+    const repoName = await getRepoName()
+    const template = `module.exports = {\npublicPath: process.env.NODE_ENV === "production" ? "/${repoName}/" : "/"\n}`
+    fs.writeFileSync(config, template, {encoding: 'utf-8'})
+  } else {
+    const file = require(config)
+    if (!file.publicPath) {
+      const repoName = await getRepoName()
+      const {EOL} = require('os')
+      const fileLines = fs.readFileSync(config, 'utf-8').split(/\r?\n/g)
+      const newLine = `publicPath: process.env.NODE_ENV === "production" ? "/${repoName}/" : "/",`
+      fileLines.splice(1, 0, newLine)
+      fs.writeFileSync(config, fileLines.join(EOL), {encoding: 'utf-8'})
+    }
+  }
+}
+
+async function getUserCredentials() {
   const {stdout: userName} = await execa("git", ["config", "user.name"])
   const {stdout: email} = await execa("git", ["config", "user.email"])
   return {
@@ -40,4 +60,15 @@ async function getUserCredentials () {
   }
 }
 
-module.exports = { addFiles, addUserNameAndEmail, extendPackage }
+async function getRepoName() {
+  const {stdout: repoUrl} = await execa('git', ['config', '--get', 'remote.origin.url'])
+  const {stdout: repoName} = await execa('basename', ['-s', '.git', repoUrl])
+  return repoName
+}
+
+module.exports = {
+  addFiles,
+  addUserNameAndEmail,
+  extendPackage,
+  createOrUpdateVueConfig
+}
--- generator/index.js
+++ generator/index.js
@@ -1,11 +1,12 @@
 const plugin = require('./helpers')
 
-module.exports = async(api, opts) => {
+module.exports = async (api, opts) => {
   plugin.extendPackage(api)
   plugin.addFiles(api, opts)
 
   api.onCreateComplete(() => {
     plugin.addUserNameAndEmail(api)
+    plugin.createOrUpdateVueConfig(api)
     api.exitLog(`Enjoy automatic deploy to github pages! 😉`)
   })
 }
